d1065b498baed34b3aed3e226aad006d8cea6b23
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-06-25 23:31:06 +0900

    .

--- cogs/crandom.py
+++ cogs/crandom.py
@@ -5,21 +5,12 @@ import random
 import re
 
 
-anydot = [
-    "...",
-    "....",
-    ".....",
-    "......",
-    ".......",
-    "........"
-]
-
-
 class CRandom(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name="crandom", description="マイクラの/randomコマンドの一部です(reset引数なし)")
+    @app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id))
     @app_commands.describe(
         mode="value : 自分にのみ表示・roll : 全員に公開",
         range="マイクラの書き方に則って範囲を書いてください"
@@ -37,77 +28,65 @@ class CRandom(commands.Cog):
         pattern1 = r"[+-]?\d{1,}..[+-]?\d{1,}"
         pattern2 = r"[+-]?\d{1,}.."
         pattern3 = r"..[+-]?\d{1,}"
+        numlist = re.findall(r'[+-]?\d+', range)
 
         if re.fullmatch(pattern1, range):
-            numlist = re.findall(r'[+-]?\d+', range)
-            start = int(numlist[0])
-            end = int(numlist[1])
-            if start >= end:
+            starts = int(numlist[0])
+            ends = int(numlist[1])
+            if starts >= ends:
                 await interaction.response.send_message("「[小さい数字]..[大きい数字]」って書いてね", ephemeral=True)
                 return
-            elif abs(start - end) + 1 > 2147483646:
+            elif abs(starts - ends) + 1 > 2147483646:
                 await interaction.response.send_message("範囲が広すぎます\n範囲の大きさは2以上2147483646以下です", ephemeral=True)
                 return
-            num = random.randint(start, end)
+            elif starts <= start:
+                await interaction.response.send_message("[小さい数字]は-2147483648より大きい数で書いてね", ephemeral=True)
+                return
+            elif ends >= end:
+                await interaction.response.send_message("[大きい数字]は2147483647より小さい数で書いてね", ephemeral=True)
+                return
+            num = random.randint(starts, ends)
 
         elif re.fullmatch(pattern2, range):
-            numlist = re.findall(r'[+-]?\d+', range)
-            start = int(numlist[0])
-            if start == end:
-                await interaction.response.send_message("「[小さい数字]..」は-2147483647以上の値を書いてね", ephemeral=True)
-                return
-            elif abs(start - end) + 1 > 2147483646:
+            starts = int(numlist[0])
+            ends = end
+            if abs(starts - ends) + 1 > 2147483646:
                 await interaction.response.send_message("範囲が広すぎます\n範囲の大きさは2以上2147483646以下です", ephemeral=True)
                 return
-            num = random.randint(start, end)
+            elif starts <= start:
+                await interaction.response.send_message("[小さい数字]は-2147483648より大きい数で書いてね", ephemeral=True)
+                return
+            elif starts >= ends:
+                await interaction.response.send_message("[小さい数字]は2147483647より小さい数で書いてね", ephemeral=True)
+                return
+            num = random.randint(starts, end)
 
         elif re.fullmatch(pattern3, range):
-            numlist = re.findall(r'[+-]?\d+', range)
-            end = int(numlist[0])
-            if start == end:
-                await interaction.response.send_message("「..[大きい数字]」は2147483646以下の値を書いてね", ephemeral=True)
-                return
-            elif abs(start - end) + 1 > 2147483646:
+            starts = start
+            ends = int(numlist[0])
+            if abs(start - ends) + 1 > 2147483646:
                 await interaction.response.send_message("範囲が広すぎます\n範囲の大きさは2以上2147483646以下です", ephemeral=True)
                 return
-            num = random.randint(start, end)
+            elif ends >= end:
+                await interaction.response.send_message("[大きい数字]は2147483647より小さい数で書いてね", ephemeral=True)
+                return
+            elif ends <= start:
+                await interaction.response.send_message("[大きい数字]は-2147483648より大きい数で書いてね", ephemeral=True)
+                return
+            num = random.randint(start, ends)
 
         else:
-            await interaction.response.send_message("範囲指定(range)が無効な記述です\n> 1..10\n> ..-10\n> 1..\nのどれかで記述してください", ephemeral=True)
+            await interaction.response.send_message("範囲指定(range)が無効な記述です\n> ・1..10\n> ・..-10\n> ・1..\nのどれかで記述してください", ephemeral=True)
             return
 
         if mode.value == "mroll":
-            await interaction.response.send_message(f"{interaction.user.display_name}は{num}を引きました ( 値の範囲は{start}から{end} )", ephemeral=False)
+            await interaction.response.send_message(f"{interaction.user.display_name}は{num}を引きました ( 値の範囲は{starts}から{ends} )", ephemeral=False, silent=True)
         elif mode.value == "mvalue":
             await interaction.response.send_message(f"乱数 : {num}", ephemeral=True)
 
-        # if anydot in range:
-        #     await interaction.response.send_message("rangeで使うドットの数は2個です\n(例 : ..-10(-10以下)・10..(10以上))・1..30(1以上30以下)")
-        # # スライスを用いて特定の文字より後ろを抽出
-        # s = '2021/03/23 05:30'
-
-        # target = '..'
-        # idx = s.find(target)
-        # r = s[idx + 1:]  # スライスで半角空白文字のインデックス＋1以降を抽出
-
-        # print(r)  # 05:30
-        # text = range
-        # str1 = ".."
-        # aaa = text[c + 1:] if (c := text.find(str1)) != -1 and text.startswith(str1) else ""
-        # bbb = text[:c] if (c := text.rfind(str1)) != -1 and text.endswith(str1) else ""
-        # ccc = text[:c] if (c := text.rfind(str1)) != -1 else ""
-        # ddd = text[c + len(str1):] if (c := text.find(str1)) != -1 else ""
-
-        # if range.content.startswith("aaa"):
-        #     number = random.randint(-2147483647, 2147483648)
-        #     startnum = 1
-        #     stopnum = 10
-        #     abcd = aaa + bbb + ccc + ddd
-
-        # if mode.value == "mroll":
-        #     await interaction.response.send_message(f"{interaction.user.display_name}は{number}を引きました ( 値の範囲は{startnum}から{stopnum} )", ephemeral=False)
-        # if mode.value == "mvalue":
-        #     await interaction.response.send_message(f"乱数 : {number}", ephemeral=True)
+    # async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+    #     if isinstance(error, app_commands.CommandOnCooldown):
+    #         await interaction.response.send_message("クールダウン中です", ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
